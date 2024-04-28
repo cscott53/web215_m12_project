4795f25c71179e7300461f49b944c63c955a8a7e
cscott53 <cscott53@email.cpcc.edu>
2024-04-28 16:08:24 -0400

    Create api.js

---
+++ server/api.js
@@ -0,0 +1,18 @@
+const express = require('express'),
+      router = express.Router(),
+      { MongoClient, ServerApiVersion } = require('mongodb'),
+      uri = 'mongodb+srv://cscott53:mernStack1@cluster0.wscobq0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+      client = new MongoClient(uri, {
+          serverApi: {
+              version: ServerApiVersion.v1,
+              strict: true,
+              deprecationErrors: true,
+          }
+      });
+/*
+I usually don't use semicolons but had to put one on
+the line above since JS thought the () on the line
+below was trying to call what was returned on the
+line above as if it were a function and throw an error
+*/
+(async()=>await client.connect())()
