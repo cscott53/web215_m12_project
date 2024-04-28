5667f025d5003e53606c8f0b9f046eb20163da1b
cscott53 <cscott53@email.cpcc.edu>
2024-04-28 15:59:50 -0400

    Added server

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require('express'),
+      path = require('path'),
+      port = process.env.PORT || 3000,
+      app = express()
+app.use(express.static(path.join(__dirname,'../client/build/')))
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'../client/build/index.html')))
+app.listen(port,()=>console.log(`Server running at port ${port}`))
