3aaedbfc40fc96e7a18ebcbd8a1c938c081adeb4
cscott53 <cscott53@email.cpcc.edu>
2024-04-29 18:10:15 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require('express'),
       router = require('./api')
 app.use('/api',router)
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname,'../client/build/')))
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'../client/build/index.html')))
 app.listen(port,()=>console.log(`Server running at port ${port}`))
